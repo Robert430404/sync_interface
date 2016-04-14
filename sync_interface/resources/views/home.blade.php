2722134dee6c179e7500bf4d2011c4738c99bf22
Robert Joseph Cox Jr <robert@studiorclv.com>
2016-04-13 23:09:19 -0700

    Initial scaffolding of the dashboard area started

--- sync_interface/resources/views/home.blade.php
+++ sync_interface/resources/views/home.blade.php
@@ -3,12 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12 col-md-offset-0">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <div class="panel-heading">Your Files</div>
                 <div class="panel-body">
-                    You are logged in!
+                    <div class="breadcrumb-wrapper">
+                        <div class="breadcrumbs">
+                            <ul>
+                                <li class="crumb-home crumb"><i class="fa fa-home"></i> <span class="label">Home</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="pane-wrapper">
+                        <div class="primary-pane pane">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
